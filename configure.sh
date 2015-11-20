a9cfa6c0d047f4b2326ec84573ab11386bd5ce59
JK <jkff90@yahoo.com>
2015-11-20 22:15:22 +0700

    rename env var

--- configure.sh
+++ configure.sh
@@ -3,6 +3,6 @@
 # Take a note that $0 does not mean "name of the program or script"
 # It is actually "how I was called"
 # So dir=`dirname $0` will return "/bin/bash" if calling by "source"
-export VERLIB_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export UVM_HOME=$VERLIB_HOME/frameworks/uvm-1.1d
-export UVC_HOME=$VERLIB_HOME/uvc
+export VERILIB_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export UVM_HOME=$VERILIB_HOME/frameworks/uvm-1.1d
+export UVC_HOME=$VERILIB_HOME/uvc
